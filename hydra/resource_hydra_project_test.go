cce4acb8b298d4ad59b2522855a14adf5ef10369
Cole Helbling <cole.e.helbling@outlook.com>
2021-05-05 11:32:44 -0700

    resource_hydra_project: test that we undo imperative changes

--- hydra/resource_hydra_project_test.go
+++ hydra/resource_hydra_project_test.go
@@ -91,6 +91,16 @@ func TestAccHydraProject_declarative(t *testing.T) {
 					testAccCheckProjectExists(resourceName),
 				),
 			},
+			// Test that state will want to be updated if declarative stuff changes in
+			// the web UI
+			{
+				Config:      testAccHydraProjectConfigDeclarative(name),
+				ExpectError: regexp.MustCompile(`Plan: 0 to add, 1 to change, 0 to destroy`),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckProjectExists(resourceName),
+					testAccCheckProjectChangeDeclFile(resourceName, name),
+				),
+			},
 		},
 	})
 }
@@ -147,6 +157,51 @@ func testAccCheckProjectExists(name string) resource.TestCheckFunc {
 		// Check to make sure the project was created
 		if get.HTTPResponse.StatusCode != http.StatusOK {
 			return fmt.Errorf("Expected project %s to be created", projectID)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckProjectChangeDeclFile(resourceName string, projectName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Resource not found for %s", resourceName)
+		}
+
+		projectID := rs.Primary.ID
+		if projectID == "" {
+			return fmt.Errorf("No ID is set for %s", resourceName)
+		}
+
+		client := testAccProvider.Meta().(*api.ClientWithResponses)
+		ctx := context.Background()
+
+		get, err := client.GetProjectIdWithResponse(ctx, projectID)
+		if err != nil {
+			return err
+		}
+		defer get.HTTPResponse.Body.Close()
+
+		// Check to make sure the project was created
+		if get.HTTPResponse.StatusCode != http.StatusOK {
+			return fmt.Errorf("Expected project %s to exist", projectID)
+		}
+
+		// Update the declarative file out-of-band to simulate a user changing the
+		// declarative config using the web UI
+		rs.Primary.Attributes["declarative.0.file"] = "bogus"
+		d := resourceHydraProject().Data(rs.Primary)
+		body := createProjectPutBody(projectName, d)
+		put, err := client.PutProjectIdWithResponse(ctx, projectID, *body)
+		if err != nil {
+			return err
+		}
+		defer put.HTTPResponse.Body.Close()
+
+		if get.HTTPResponse.StatusCode != http.StatusOK {
+			return fmt.Errorf("Expected project %s to be updated", projectID)
 		}
 
 		return nil
